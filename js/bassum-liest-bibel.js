0ae986685f5c5b619b068828b7e919f772109d31
unknown <c.r@arcormail.de>
2017-05-31 21:39:46 +0200

    initial commit

---
+++ js/bassum-liest-bibel.js
@@ -0,0 +1,34 @@
+$(function () {
+
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+    //$('#order-form').validator();
+});
+
+
+
+
+//TODO refactor
+
+$('#newsletter-method-select').change(function() {
+  refreshEmailInput();
+});
+
+$('#newsletter-checkbox').change(function() {
+  refreshNewsletterMethodSelect();
+  refreshEmailInput();
+});
+
+
+function refreshNewsletterMethodSelect(){
+  var enableMethodSelect = $('#newsletter-checkbox').prop('checked');
+  $("#newsletter-method-select").prop('disabled', !enableMethodSelect);
+}
+
+function refreshEmailInput(){
+  //box == checked && select = email
+  var enableEmailInput = $('#newsletter-checkbox').prop('checked') && $('#newsletter-method-select').val() == "email";
+  $("#email-input").prop('disabled', !enableEmailInput);
+}
